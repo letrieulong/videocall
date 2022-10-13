b89705b620d8a3bc98b2b2d6b38dd90ee5e61c52
letrieulong <letrieulong412000@gmail.com>
2022-10-13 17:17:12 +0700

    ''

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {})
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port 3000...")
 })
 
